70693189f343a94562e54ae623f884d231060a1c
chen <you@example.com>
2022-05-17 18:22:38 +0800

    添加项目文件。

---
+++ 刷题答案/刷题答案.c
@@ -0,0 +1,11 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+    int q = 0;
+    int w = 0;
+    int e = 0;
+    scanf("%d%d%d", &q, &w, &e);
+    printf("%d\n", w);
+    return 0;
+}
